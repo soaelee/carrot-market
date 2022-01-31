c5e69732c692ed21f0c5d7ed41315c853ad10ff2
soaelee <soae0923@gmail.com>
2022-01-31 23:58:57 +0900

    Project Setup

---
+++ README.md
@@ -0,0 +1,8 @@
+# Carrot Market
+
+Serverless Carrot Market Clone using NextJS, Tailwind, Prisma, PlanetScale and Cloudflare
+
+## tailwindcss setup
+
+> 1. npm i -D tailwindcss postcss autoprefixer
+> 2. npx tailwindcss init -p
---
+++ pages/index.tsx
@@ -0,0 +1,10 @@
+import type { NextPage } from "next";
+const Home: NextPage = () => {
+  return (
+    <div className="bg-red-500">
+      <h1 className="text-black-500">it works</h1>
+    </div>
+  );
+};
+
+export default Home;
